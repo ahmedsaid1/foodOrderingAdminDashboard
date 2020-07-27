72bf50561bc38821b4c39a900f56a62ed72f7872
AhmedSaid <ahmedsaid202088@gmail.com>
2020-07-27 20:31:10 +0300

    new change

--- AdminWebService/src/main/java/com/eprogs/rest/AdminWebServiceApplication.java
+++ AdminWebService/src/main/java/com/eprogs/rest/AdminWebServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-
+//lol
 @SpringBootApplication
 @ComponentScan({"com.eprogs"})
 @EntityScan("com.eprogs.spring.dal.entity")
